8588a946b348fa6d6a7e55eb13cc85b1d12af10f
franciszekostrowski <franekostrowski@protonmail.com>
2022-07-15 22:49:00 +0200

    feat: add route protection

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import DefaultLayout from "common/layouts/Default";
 import Home from "modules/Home";
+import { withAuthRequired } from "@supabase/supabase-auth-helpers/nextjs";
+
+const getServerSideProps = withAuthRequired({ redirectTo: "/signin" })
 
 export default function HomePage() {
   return (
