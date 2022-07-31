1809ae2873ace957ff55390e93bbedb18fc7e46a
franciszekostrowski <franekostrowski@protonmail.com>
2022-07-31 19:34:19 +0200

    feat: improve account page

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -5,11 +5,11 @@ import Avatar from "@mui/joy/Avatar";
 import generateInitials from "common/utils/generateInitials";
 import Typography from "@mui/joy/Typography";
 import Box from "@mui/joy/Box";
-import ListItem from "@mui/joy/ListItem";
-import List from "@mui/joy/List";
-import ListItemDecorator from "@mui/joy/ListItemDecorator";
-import ListItemContent from "@mui/joy/ListItemContent";
+import Input from "@mui/joy/Input"
 import EditRounded from "@mui/icons-material/EditRounded";
+import Radio from "@mui/joy/Radio";
+import RadioGroup from "@mui/joy/RadioGroup";
+import { PersonRounded, SettingsRounded } from "@mui/icons-material";
 
 export const getServerSideProps = withAuthRequired({ redirectTo: "/signin" });
 
@@ -19,42 +19,36 @@ export default function AccountPage({ user }: { user: User }) {
   return (
     <DefaultLayout>
       <Box display="flex" alignItems="center" flexDirection="column">
-        <Avatar sx={{ width: 140, height: 140, mb: 4 }} size="lg">
+        <Avatar sx={{ width: 140, height: 140 }} size="lg">
           <Typography level="display2">
             {generateInitials(firstName, lastName)}
           </Typography>
         </Avatar>
-
-        <List sx={{ width: "100%" }} size="lg">
-          <ListItem>
-            <ListItemDecorator>
-              <EditRounded />
-            </ListItemDecorator>
-            <ListItemContent
-              sx={{ display: "flex", justifyContent: "space-between", gap: 2 }}
-            >
-              <Typography>First name</Typography>
-              <Typography fontWeight="md" textColor="neutral.400">
-                {" " + firstName}
-              </Typography>
-            </ListItemContent>
-          </ListItem>
-
-          <ListItem>
-            <ListItemDecorator>
-              <EditRounded />
-            </ListItemDecorator>
-            <ListItemContent
-              sx={{ display: "flex", justifyContent: "space-between", gap: 2 }}
-            >
-              <Typography>Last name</Typography>
-              <Typography fontWeight="md" textColor="neutral.400">
-                {" " + lastName}
-              </Typography>
-            </ListItemContent>
-          </ListItem>
-        </List>
       </Box>
+
+      <Typography startDecorator={<PersonRounded />} level="h4" mt={4} mb={2}>Account</Typography>
+
+      <Typography gutterBottom>First name</Typography>
+      <Input startDecorator={<EditRounded />} variant="soft" value={firstName}></Input>
+
+      <Typography gutterBottom mt={2}>Last name</Typography>
+      <Input startDecorator={<EditRounded />} variant="soft" value={lastName}></Input>
+
+      <Typography mt={4} mb={2} level="h4" startDecorator={<SettingsRounded />}>Preferences</Typography>
+
+      <Typography level="h5" mb={2}>Mode</Typography>
+      <RadioGroup row color="neutral" size="lg">
+        <Radio value="System" label="System" />
+        <Radio value="Light" label="Light" />
+        <Radio value="Dark" label="Dark" />
+      </RadioGroup>
+
+      <Typography level="h5" mt={4} mb={2}>Units</Typography>
+      <RadioGroup row color="neutral" size="lg">
+        <Radio value="Imperial" label="Imperial" />
+        <Radio value="Metric" label="Metric" />
+      </RadioGroup>
+
     </DefaultLayout>
   );
 }
