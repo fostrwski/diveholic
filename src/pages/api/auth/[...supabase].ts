5e7c40cfb720b9ccf9f0b274a904752c178840b1
franciszekostrowski <franekostrowski@protonmail.com>
2022-08-01 19:31:18 +0200

    feat: remove unnecessary prop

--- src/pages/api/auth/[...supabase].ts
+++ src/pages/api/auth/[...supabase].ts
@@ -2,5 +2,4 @@ import { handleAuth } from "@supabase/auth-helpers-nextjs";
 
 export default handleAuth({
   logout: { returnTo: "/signin" },
-  cookieOptions: { lifetime: 1 * 365 * 24 * 60 * 60 },
 });
