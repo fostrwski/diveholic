c50b361861784712dce15d395f66243a3d273c68
franciszekostrowski <franekostrowski@protonmail.com>
2022-08-04 14:31:29 +0200

    feat: add new page for adding dives, filled with dummy content

--- src/modules/Dives/New.tsx
+++ src/modules/Dives/New.tsx
@@ -1,12 +1,57 @@
+import { FlagRounded, PublicRounded } from "@mui/icons-material";
+import AddRounded from "@mui/icons-material/AddRounded";
+import Box from "@mui/joy/Box";
+import Chip from "@mui/joy/Chip";
+import Grid from "@mui/joy/Grid";
+import TextField from "@mui/joy/TextField";
 import Typography from "@mui/joy/Typography";
 import React from "react";
 
 const New: React.FC = () => {
   return (
     <>
-      <Typography level="h5" component="h2">
-        Log new dive
+      <Chip startDecorator={<AddRounded />} variant="soft" size="lg">
+        Log dive
+      </Chip>
+
+      <Typography level="h6" component="p" textColor="neutral.600" mt={2}>
+        Fill in information about your dive 🤿
       </Typography>
+
+      <Box
+        component="form"
+        mt={4}
+        sx={{ display: "flex", gap: 2, flexDirection: "column" }}
+      >
+        <Typography level="h4" component="p">
+          Basic information
+        </Typography>
+
+        <Box display="flex" justifyContent="space-between" gap={2}>
+          <TextField type="Date" label="Date" fullWidth />
+          <TextField type="time" label="Time" fullWidth />
+        </Box>
+
+        <Grid mt={2} spacing={2} container>
+          <Grid xs={6}>
+            <TextField
+              type="text"
+              label="Country"
+              startDecorator={<PublicRounded />}
+            />
+          </Grid>
+          <Grid xs={6}>
+            <TextField type="text" label="City" />
+          </Grid>
+          <Grid xs={12}>
+            <TextField
+              type="text"
+              label="Dive center"
+              startDecorator={<FlagRounded />}
+            />
+          </Grid>
+        </Grid>
+      </Box>
     </>
   );
 };
---
+++ src/pages/dives/new.tsx
@@ -0,0 +1,11 @@
+import DefaultLayout from "common/layouts/Default"
+import New from "modules/Dives/New"
+import React from "react"
+
+export default function NewPage() {
+  return (
+  <DefaultLayout>
+      <New />
+    </DefaultLayout>
+  )
+}
