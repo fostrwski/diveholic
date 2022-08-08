1d55977ceba7e861156e258c00f56dcd971a5900
franciszekostrowski <franekostrowski@protonmail.com>
2022-08-08 19:03:23 +0200

    feat: temporarily disable some properties on the Dive type

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,31 +1,32 @@
 export type Dive = {
-  id: number;
-  date: Date;
-  length: number;
-  maxDepth: number;
-  location: {
-    country: string;
-    city: string;
-    diveCenter: string;
-  };
-  temperature: {
-    air: number;
-    water: {
-      average: number;
-      minimum: number;
-    };
-  };
-  tanks: {
-    count: number;
-    type: string;
-    capacity: number;
-  };
-  water: string;
-  weights: number;
-  gear: {
-    exposureProtection: {
-      type: string;
-      thickness: number;
-    };
-  };
+  // id: number;
+  date: string;
+  time: string;
+  // length: number;
+  // maxDepth: number;
+  // location: {
+  //   country: string;
+  //   city: string;
+  //   diveCenter: string;
+  // };
+  // temperature: {
+  //   air: number;
+  //   water: {
+  //     average: number;
+  //     minimum: number;
+  //   };
+  // };
+  // tanks: {
+  //   count: number;
+  //   type: string;
+  //   capacity: number;
+  // };
+  // water: string;
+  // weights: number;
+  // gear: {
+  //   exposureProtection: {
+  //     type: string;
+  //     thickness: number;
+  //   };
+  // };
 };
