7f23945d1a40d91f8bd087b873f01836e07a4599
franciszekostrowski <franekostrowski@protonmail.com>
2022-08-14 16:52:06 +0200

    fix: add dependency to useEffect hook

--- src/modules/SignIn/index.tsx
+++ src/modules/SignIn/index.tsx
@@ -27,7 +27,7 @@ const SignIn: React.FC = () => {
     if (user) {
       router.push("/");
     }
-  }, []);
+  }, [user]);
 
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
