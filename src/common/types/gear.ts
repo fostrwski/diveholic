bbcb79689940ba776e0590c458b9f76a337cc3c4
franciszekostrowski <franekostrowski@protonmail.com>
2022-08-16 23:18:44 +0200

    feat: add new types

---
+++ src/common/types/gear.ts
@@ -0,0 +1,8 @@
+type Gear = {
+  exposureProtection: {
+    type: string;
+    thickness: number | null;
+  };
+};
+
+export default Gear;
---
+++ src/common/types/index.ts
@@ -0,0 +1,28 @@
+import Gear from "./gear";
+
+export type Dive = {
+  id?: number;
+  date: string;
+  time: string;
+  location: {
+    country: string;
+    city: string;
+    diveCenter: string;
+  };
+  length: number | null;
+  maxDepth: number | null;
+  weights: number | null;
+  water: string;
+  temperature: {
+    air: number | null;
+    water: {
+      average: number | null;
+      minimum: number | null;
+    };
+  };
+  tanks: {
+    count: number;
+    type: string;
+  };
+  gear: Gear;
+};
