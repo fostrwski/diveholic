836b5738d1a7a49bd11ddd77807252901c002a1c
franciszekostrowski <franekostrowski@protonmail.com>
2022-08-22 00:14:24 +0200

    feat: add and setup cypress

---
+++ cypress/e2e/dives/new.cy.ts
@@ -0,0 +1,10 @@
+describe("empty spec", () => {
+  beforeEach(() => {
+    cy.viewport("iphone-x");
+    cy.visit("http://localhost:3000");
+  });
+
+  it("Tests tests", () => {
+    cy.log("Hello");
+  });
+});
