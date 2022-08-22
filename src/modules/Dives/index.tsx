8822713403f80e8b7b321bb01f39848b4ab820e8
franciszekostrowski <franekostrowski@protonmail.com>
2022-08-22 22:53:25 +0200

    style: make it griddy!

--- src/modules/Dives/index.tsx
+++ src/modules/Dives/index.tsx
@@ -1,3 +1,4 @@
+import Grid from "@mui/joy/Grid";
 import DiveCard from "common/components/DiveCard";
 import type { Dive } from "common/types";
 import React from "react";
@@ -8,11 +9,13 @@ interface Dives {
 
 const Dives: React.FC<Dives> = ({ data }) => {
   return (
-    <>
+    <Grid container gap={2}>
       {data.map((dive: Dive) => (
-        <DiveCard key={dive.id} dive={dive} />
+        <Grid xs={12}>
+          <DiveCard key={dive.id} dive={dive} />
+        </Grid>
       ))}
-    </>
+    </Grid>
   );
 };
 
