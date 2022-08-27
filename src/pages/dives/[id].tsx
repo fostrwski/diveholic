46700ab154496ae741057bfd557675d80eb3b0d7
franciszekostrowski <franekostrowski@protonmail.com>
2022-08-27 20:22:30 +0200

    feat: add route protection

--- src/pages/dives/[id].tsx
+++ src/pages/dives/[id].tsx
@@ -1,5 +1,10 @@
+import { withPageAuth } from "@supabase/auth-helpers-nextjs";
 import DefaultLayout from "common/layouts/Default";
 import Dive from "modules/Dives/Dive";
+
+export const getServerSideProps = withPageAuth({
+  redirectTo: "/signin",
+});
 
 export default function DivePage() {
   return (
