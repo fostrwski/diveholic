7e6621d059a64973aef76587ee18f56fc5748a7f
franciszekostrowski <franekostrowski@protonmail.com>
2022-08-29 21:06:38 +0200

    feat: change ListDivider's component and some styling

--- src/common/components/Separator/index.tsx
+++ src/common/components/Separator/index.tsx
@@ -2,7 +2,7 @@ import ListDivider from "@mui/joy/ListDivider";
 import React from "react";
 
 const Separator: React.FC = () => {
-  return <ListDivider sx={{ width: "100%", height: "2px" }} />;
+  return <ListDivider component="hr" sx={{ width: "100%", height: "2px" }} />;
 };
 
 export default Separator;
