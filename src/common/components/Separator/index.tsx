df948afb707946109592f23d746d64a06550c99d
franciszekostrowski <franekostrowski@protonmail.com>
2022-08-29 19:32:56 +0200

    feat: add separator component

---
+++ src/common/components/Separator/index.tsx
@@ -0,0 +1,8 @@
+import ListDivider from "@mui/joy/ListDivider";
+import React from "react";
+
+const Separator: React.FC = () => {
+  return <ListDivider sx={{ width: "100%", height: "2px" }} />;
+};
+
+export default Separator;
