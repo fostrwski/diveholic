a0917b40bc6af1c28254f2e9b21e07f42aa13091
franciszekostrowski <franekostrowski@protonmail.com>
2022-09-21 11:46:29 +0200

    feat: edit initialState to match new type declaration

--- src/modules/Dives/New/diveInitialState.ts
+++ src/modules/Dives/New/diveInitialState.ts
@@ -1,7 +1,7 @@
 import { DiveFlattened } from "common/types";
 
 const diveInitialState: DiveFlattened = {
-  date: new Date(),
+  date: null,
   time: "",
   locationCountryName: "",
   locationCountryCode: "",
