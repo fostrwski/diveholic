8957ddf56ace10df1155a6423ba4cf841031574c
franciszekostrowski <franekostrowski@protonmail.com>
2022-09-22 17:04:53 +0200

    chore: remove function thats no longer in use

--- src/modules/Dives/New/DatePicker/generateWeekDaysArrayStartingWithDay.ts
+++
@@ -1,9 +0,0 @@
-import weekdays from "./weekdays";
-
-export default function generateWeekDaysArrayStartingWithDay(day: string) {
-  const weekArray = [
-    ...weekdays.slice(weekdays.indexOf(day)),
-    ...weekdays.slice(0, weekdays.indexOf(day)),
-  ];
-  return weekArray;
-}
