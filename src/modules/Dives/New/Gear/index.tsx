d364b30c839fd2f570688a23591693064ac429d3
franciszekostrowski <franekostrowski@protonmail.com>
2022-10-02 17:57:55 +0200

    feat: handle show more button

--- src/modules/Dives/New/Gear/index.tsx
+++ src/modules/Dives/New/Gear/index.tsx
@@ -1,10 +1,11 @@
 import LineWeightRounded from "@mui/icons-material/LineWeightRounded";
 import NumbersRounded from "@mui/icons-material/NumbersRounded";
+import Button from "@mui/joy/Button";
 import Grid from "@mui/joy/Grid";
 import TextField from "@mui/joy/TextField";
 import Typography from "@mui/joy/Typography";
 import TextSeparator from "common/components/TextSeparator";
-import React from "react";
+import React, { useState } from "react";
 
 import type { ComponentWithTextFieldsProps } from "../types";
 
@@ -12,6 +13,12 @@ const Gear: React.FC<ComponentWithTextFieldsProps> = ({
   dive,
   handleTextFieldChange,
 }) => {
+  const [showMore, setShowMore] = useState<boolean>(false);
+
+  const handleClick = () => {
+    setShowMore(!showMore);
+  };
+
   return (
     <>
       <TextSeparator sx={{ mt: 8 }}>Gear</TextSeparator>
@@ -76,6 +83,56 @@ const Gear: React.FC<ComponentWithTextFieldsProps> = ({
           />
         </Grid>
       </Grid>
+
+      {showMore && (
+        <>
+          <Typography
+            mb={2}
+            mt={4}
+            component="p"
+            textColor="GrayText"
+            fontWeight="md"
+          >
+            Other gear
+          </Typography>
+          <Grid container spacing={2} justifyContent="space-between">
+            <Grid xs={6}>
+              <TextField
+                type="text"
+                name="bcd"
+                label="BCD"
+                onChange={(e) => handleTextFieldChange(e, "gearBcd")}
+              />
+            </Grid>
+            <Grid xs={6}>
+              <TextField
+                type="text"
+                name="fins"
+                label="Fins"
+                onChange={(e) => handleTextFieldChange(e, "gearFins")}
+              />
+            </Grid>
+            <Grid xs={6}>
+              <TextField
+                type="text"
+                name="regulator"
+                label="Regulator"
+                onChange={(e) => handleTextFieldChange(e, "gearRegulator")}
+              />
+            </Grid>
+          </Grid>
+        </>
+      )}
+
+      <Button
+        sx={{ mt: 6 }}
+        variant="outlined"
+        color="neutral"
+        fullWidth
+        onClick={handleClick}
+      >
+        {showMore ? "Show less" : "Show more"}
+      </Button>
     </>
   );
 };
