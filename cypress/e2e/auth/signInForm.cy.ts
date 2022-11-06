76237d87b21d0baae1562b2a391f9611b0d817dc
franciszekostrowski <franekostrowski@protonmail.com>
2022-11-06 13:47:56 +0100

    feat: tests sign in form

---
+++ cypress/e2e/auth/signInForm.cy.ts
@@ -0,0 +1,17 @@
+describe("Sign in form", () => {
+  beforeEach(() => {
+    cy.viewport("iphone-x");
+    cy.visit("http://localhost:3000");
+  });
+
+  it("Can submit a valid form", () => {
+    cy.log("Filling out email");
+    cy.get('[data-cy="email"]').type(Cypress.env("test_user_1_email"));
+    cy.log("Filling out password");
+    cy.get('[data-cy="password"]').type(Cypress.env("test_user_1_password"));
+    cy.get('[data-cy="submit"]').click();
+    cy.contains(`Hi ${Cypress.env("test_user_1_firstname")}`);
+  });
+});
+
+export {};
