d9642cea96de69c286a5081ef36630567fbe7e76
franciszekostrowski <franekostrowski@protonmail.com>
2022-11-16 13:17:05 +0100

    feat: remove global context provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import { CssVarsProvider } from "@mui/joy/styles";
 import { UserProvider } from "@supabase/auth-helpers-react";
-import { NewDiveContextProvider } from "common/context/NewDive";
 import theme from "common/theme";
 import { supabase } from "common/utils/supabaseClient";
 import type { AppProps } from "next/app";
@@ -13,9 +12,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         defaultMode="system"
         disableTransitionOnChange
       >
-        <NewDiveContextProvider>
-          <Component {...pageProps} />
-        </NewDiveContextProvider>
+        <Component {...pageProps} />
       </CssVarsProvider>
     </UserProvider>
   );
