c37f962474f601756bb03e906c676d7742434979
franciszekostrowski <franekostrowski@protonmail.com>
2022-11-16 13:46:45 +0100

    feat: wrap page with its own context provider

--- src/pages/dives/new.tsx
+++ src/pages/dives/new.tsx
@@ -1,4 +1,5 @@
 import { User, withPageAuth } from "@supabase/auth-helpers-nextjs";
+import { NewDiveContextProvider } from "common/context/NewDive";
 import DefaultLayout from "common/layouts/Default";
 import New from "modules/Dives/New";
 import React from "react";
@@ -8,7 +9,9 @@ export const getServerSideProps = withPageAuth({ redirectTo: "/signin" });
 export default function NewPage({ user }: { user: User }) {
   return (
     <DefaultLayout>
-      <New user={user} />
+      <NewDiveContextProvider>
+        <New user={user} />
+      </NewDiveContextProvider>
     </DefaultLayout>
   );
 }
