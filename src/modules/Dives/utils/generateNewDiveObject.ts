3f3f4fd97ea29f4dbcd64c3d1f2db44e40b9f0e8
franciszekostrowski <franekostrowski@protonmail.com>
2022-11-16 13:45:36 +0100

    refactor: change location :)

---
+++ src/modules/Dives/utils/generateNewDiveObject.ts
@@ -0,0 +1,51 @@
+import type { Dive, DiveFlattened } from "common/types";
+
+export default function generateNewDiveObject(dive: DiveFlattened): Dive {
+  const newDive: Dive = {
+    date: dive.date!,
+    time: dive.time,
+    location: {
+      country: {
+        name: dive.locationCountryName,
+        code: dive.locationCountryCode,
+        flagEmoji: dive.locationCountryFlagEmoji,
+      },
+      city: dive.locationCity,
+      diveCenter: dive.locationDiveCenter,
+    },
+    type: dive.type,
+    length: dive.length,
+    units: dive.units,
+    depth: {
+      average: dive.depthAverage,
+      max: dive.depthMax,
+    },
+    weights: {
+      taken: dive.weightsTaken,
+      ammount: dive.weightsAmmount,
+    },
+    water: dive.water,
+    temperature: {
+      air: dive.temperatureAir,
+      water: {
+        surface: dive.temperatureWaterSurface,
+        bottom: dive.temperatureWaterBottom,
+      },
+    },
+    gear: {
+      exposureProtection: {
+        type: dive.gearExposureProtectionType,
+        thickness: dive.gearExposureProtectionThickness,
+      },
+      tanks: {
+        count: dive.gearTanksCount,
+        type: dive.gearTanksType,
+      },
+      bcd: dive.gearBcd,
+      regulator: dive.gearRegulator,
+      fins: dive.gearFins,
+    },
+  };
+
+  return newDive;
+}
