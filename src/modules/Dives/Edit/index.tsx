7b196a773a4f54213f7343955cc87fd9380ce2cd
franciszekostrowski <franekostrowski@protonmail.com>
2022-11-27 21:27:00 +0100

    feat: update context on page load with currently edited dive

--- src/modules/Dives/Edit/index.tsx
+++ src/modules/Dives/Edit/index.tsx
@@ -1,14 +1,39 @@
 import type { User } from "@supabase/auth-helpers-nextjs";
-import React from "react";
+import { useUser } from "@supabase/auth-helpers-react";
+import { useNewDiveContext } from "common/context/NewDive";
+import type { Dive, DiveFlattened } from "common/types";
+import { supabase } from "common/utils/supabaseClient";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
 
 import Form from "../components/Form";
+import generateDiveFlattenedObject from "../utils/generateDiveFlattenedObject";
 import Header from "./Header";
 
-interface EditProps {
-  user: User;
-}
+const Edit: React.FC = () => {
+  const { user } = useUser();
+  const router = useRouter();
+  const { id: diveId } = router.query;
+  const { setNewDive } = useNewDiveContext();
 
-const Edit: React.FC<EditProps> = ({ user }) => {
+  useEffect(() => {
+    const getDiveById = async () => {
+      const { data, error } = await supabase
+        .from<Dive>("dives")
+        .select("*")
+        .match({ id: diveId });
+
+      if (error) return console.error(error);
+
+      if (data) {
+        const dive = data[0];
+        setNewDive(generateDiveFlattenedObject(dive));
+      }
+    };
+
+    if (user) getDiveById();
+  }, [user]);
+
   return (
     <>
       <Header />
