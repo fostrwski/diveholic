424ed2485257ecec38b10e193c6be657efd459bc
franciszekostrowski <franekostrowski@protonmail.com>
2022-12-01 19:25:15 +0100

    feat: remove custom formik hook (fail)

--- src/modules/Dives/components/Form/hooks/useFormikContext.ts
+++
@@ -1,7 +0,0 @@
-import { useFormikContext as useDefaultFormikContext } from "formik";
-
-import initialValues from "../initialValues";
-
-const useFormikContext = useDefaultFormikContext<typeof initialValues>();
-
-export default useFormikContext;
