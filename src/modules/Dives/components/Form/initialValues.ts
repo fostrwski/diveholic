101bd97755dce103f8d213a694c7380ad15e2897
franciszekostrowski <franekostrowski@protonmail.com>
2022-12-01 19:20:19 +0100

    feat: add initialValues file and implement it

---
+++ src/modules/Dives/components/Form/initialValues.ts
@@ -0,0 +1,30 @@
+import type { DiveFlattened } from "common/types";
+
+const initialValues: DiveFlattened = {
+  date: "",
+  locationCountryName: "",
+  locationCountryCode: "",
+  locationCountryFlagEmoji: "",
+  locationCity: "",
+  locationDiveCenter: "",
+  length: 0,
+  type: "boat",
+  units: "metric",
+  depthMax: 0,
+  depthAverage: 0,
+  weightsTaken: 0,
+  weightsAmmount: "perfect",
+  water: "fresh",
+  temperatureAir: null,
+  temperatureWaterSurface: null,
+  temperatureWaterBottom: null,
+  gearExposureProtectionType: "",
+  gearExposureProtectionThickness: 0,
+  gearTanksCount: 1,
+  gearTanksType: "",
+  gearBcd: "",
+  gearRegulator: "",
+  gearFins: "",
+};
+
+export default initialValues;
--- src/pages/dives/new.tsx
+++ src/pages/dives/new.tsx
@@ -1,10 +1,10 @@
 import { User, withPageAuth } from "@supabase/auth-helpers-nextjs";
 import { NewDiveContextProvider } from "common/context/NewDive";
-import { newDiveInitialState } from "common/context/NewDive/initialState";
 import DefaultLayout from "common/layouts/Default";
 import { supabase } from "common/utils/supabaseClient";
 import { Formik } from "formik";
 import New from "modules/Dives/New";
+import initialValues from "modules/Dives/components/Form/initialValues";
 import generateNewDiveObject from "modules/Dives/utils/generateNewDiveObject";
 import React from "react";
 
@@ -14,7 +14,7 @@ export default function NewPage({ user }: { user: User }) {
   return (
     <DefaultLayout>
       <Formik
-        initialValues={newDiveInitialState}
+        initialValues={initialValues}
         onSubmit={(values, actions) => {
           console.log(values);
           actions.setSubmitting(false);
