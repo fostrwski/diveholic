ccb9789b81b2d3c9da826927485bb20515910986
franciszekostrowski <franekostrowski@protonmail.com>
2022-12-02 14:08:14 +0100

    feat: edit submit functions and implement react-hook-form

--- src/modules/Dives/Edit/index.tsx
+++ src/modules/Dives/Edit/index.tsx
@@ -1,44 +1,43 @@
-import type { User } from "@supabase/auth-helpers-nextjs";
 import { useUser } from "@supabase/auth-helpers-react";
-import { useNewDiveContext } from "common/context/NewDive";
 import type { Dive, DiveFlattened } from "common/types";
 import { supabase } from "common/utils/supabaseClient";
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
+import { useFormContext } from "react-hook-form";
 
 import Form from "../components/Form";
-import generateDiveFlattenedObject from "../utils/generateDiveFlattenedObject";
+import generateDiveFlattenedObject from "../components/Form/utils/generateDiveFlattenedObject";
 import Header from "./Header";
 
 const Edit: React.FC = () => {
   const { user } = useUser();
   const router = useRouter();
   const { id: diveId } = router.query;
-  const { setNewDive } = useNewDiveContext();
+  const onSubmit = (data) => console.log(data);
+  const { handleSubmit } = useFormContext();
 
-  useEffect(() => {
-    const getDiveById = async () => {
-      const { data, error } = await supabase
-        .from<Dive>("dives")
-        .select("*")
-        .match({ id: diveId });
+  // useEffect(() => {
+  //   const getDiveById = async () => {
+  //     const { data, error } = await supabase
+  //       .from<Dive>("dives")
+  //       .select("*")
+  //       .match({ id: diveId });
 
-      if (error) return console.error(error);
+  //     if (error) return console.error(error);
 
-      if (data) {
-        const dive = data[0];
-        setNewDive(generateDiveFlattenedObject(dive));
-      }
-    };
+  //     if (data) {
+  //       const dive = data[0];
+  //     }
+  //   };
 
-    if (user) getDiveById();
-  }, [user]);
+  //   if (user) getDiveById();
+  // }, [user]);
 
   return (
     <>
       <Header />
 
-      <Form onSubmit={() => {}} />
+      <Form onSubmit={handleSubmit(onSubmit)} />
     </>
   );
 };
--- src/modules/Dives/New/index.tsx
+++ src/modules/Dives/New/index.tsx
@@ -1,11 +1,10 @@
 import type { User } from "@supabase/auth-helpers-nextjs";
-import { useNewDiveContext } from "common/context/NewDive";
 import { supabase } from "common/utils/supabaseClient";
 import React from "react";
 import { useFormContext } from "react-hook-form";
 
 import Form from "../components/Form";
-import generateNewDiveObject from "../utils/generateNewDiveObject";
+import generateNewDiveObject from "../components/Form/utils/generateNewDiveObject";
 import Header from "./Header";
 
 interface NewProps {
@@ -13,19 +12,18 @@ interface NewProps {
 }
 
 const New: React.FC<NewProps> = ({ user }) => {
-  const { newDive } = useNewDiveContext();
-  const { handleSubmit } = useFormContext();
+  const { handleSubmit, getValues } = useFormContext();
   const onSubmit = (data) => console.log(data);
 
-  const customSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const diveObject = generateNewDiveObject(newDive);
+  // const customSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //   const diveObject = generateNewDiveObject(getValues());
 
-    const { error } = await supabase
-      .from("dives")
-      .insert({ user_id: user.id, ...diveObject });
-    if (error) console.error(error);
-  };
+  //   const { error } = await supabase
+  //     .from("dives")
+  //     .insert({ user_id: user.id, ...diveObject });
+  //   if (error) console.error(error);
+  // };
 
   return (
     <>
