fd4628210b9d76a25c1cb520996e0466a6328875
franciszekostrowski <franekostrowski@protonmail.com>
2022-12-02 14:12:06 +0100

    feat: implement new context provider

--- src/pages/dives/edit/[id].tsx
+++ src/pages/dives/edit/[id].tsx
@@ -1,17 +1,20 @@
 import { withPageAuth } from "@supabase/auth-helpers-nextjs";
-import { NewDiveContextProvider } from "common/context/NewDive";
 import DefaultLayout from "common/layouts/Default";
 import Edit from "modules/Dives/Edit";
+import defaultValues from "modules/Dives/components/Form/defaultValues";
+import { FormFields } from "modules/Dives/components/Form/types";
 import React from "react";
+import { FormProvider, useForm } from "react-hook-form";
 
 export const getServerSideProps = withPageAuth({ redirectTo: "/signin" });
 
 export default function EditPage() {
+  const methods = useForm<FormFields>({ defaultValues });
   return (
     <DefaultLayout>
-      <NewDiveContextProvider>
+      <FormProvider {...methods}>
         <Edit />
-      </NewDiveContextProvider>
+      </FormProvider>
     </DefaultLayout>
   );
 }
