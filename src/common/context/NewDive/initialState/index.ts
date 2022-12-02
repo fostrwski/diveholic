14b2754ad0096ed29f7acbc22c773d66002bc38d
franciszekostrowski <franekostrowski@protonmail.com>
2022-12-02 13:58:17 +0100

    chore: remove context directory

--- src/common/context/NewDive/initialState/index.ts
+++
@@ -1,4 +0,0 @@
-import newDiveInitialState from "./newDive";
-import newDiveContextInitialState from "./newDiveContext";
-
-export { newDiveInitialState, newDiveContextInitialState };
--- src/common/context/NewDive/initialState/types.ts
+++
@@ -1,9 +0,0 @@
-import { DiveFlattened } from "common/types";
-
-interface NewDiveContextInitialState {
-  newDive: DiveFlattened;
-  updateNewDiveProp: (prop: keyof DiveFlattened, value: any) => void;
-  setNewDive: React.Dispatch<React.SetStateAction<DiveFlattened>>;
-}
-
-export type { NewDiveContextInitialState };
