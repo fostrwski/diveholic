7f67da0d84e90e9a8bbf359cd3c44ceaa44a5480
franciszekostrowski <franekostrowski@protonmail.com>
2022-12-13 13:45:07 +0100

    feat: experiment with pwa

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,26 @@
 import { getInitColorSchemeScript } from "@mui/joy/styles";
-import Document, {
-  Head, Html, Main, NextScript
-} from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head></Head>
+        <Head>
+          <meta name="application-name" content="Diveholic" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-title" content="Diveholic" />
+          <meta
+            name="description"
+            content="Dive log built for the modern age"
+          />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="theme-color" content="#fff" />
+        </Head>
         <body>
           {getInitColorSchemeScript({ defaultMode: "system" })}
           <Main />
