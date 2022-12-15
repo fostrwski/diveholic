212ca2632c1b42b9decc0c364d08aad71c8cb0a4
franciszekostrowski <franekostrowski@protonmail.com>
2022-12-15 22:24:27 +0100

    feat: disable PWA in development environment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
-  skipWaiting: true
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development"
 });
 
 module.exports = withPWA({
