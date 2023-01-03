3d63fba63c2fd0d4296cd2f1aa5451c5e647411c
franciszekostrowski <franekostrowski@protonmail.com>
2023-01-03 23:53:50 +0100

    feat: lazy load date picker modal (trynna solve performance issues)

--- src/modules/Dives/components/Form/DatePicker/index.tsx
+++ src/modules/Dives/components/Form/DatePicker/index.tsx
@@ -1,27 +1,18 @@
-import { ChevronLeftRounded, ChevronRightRounded } from "@mui/icons-material";
 import CalendarTodayRounded from "@mui/icons-material/CalendarTodayRounded";
 import EditRounded from "@mui/icons-material/EditRounded";
-import ErrorOutlineRounded from "@mui/icons-material/ErrorOutlineRounded";
-import { FormHelperText, IconButton } from "@mui/joy";
 import Avatar from "@mui/joy/Avatar";
 import Box from "@mui/joy/Box";
-import Button from "@mui/joy/Button";
 import FormControl from "@mui/joy/FormControl";
+import FormHelperText from "@mui/joy/FormHelperText";
 import FormLabel from "@mui/joy/FormLabel";
-import Grid from "@mui/joy/Grid";
 import Link from "@mui/joy/Link";
-import Modal from "@mui/joy/Modal";
-import ModalDialog from "@mui/joy/ModalDialog";
-import Radio from "@mui/joy/Radio";
-import RadioGroup from "@mui/joy/RadioGroup";
 import Typography from "@mui/joy/Typography";
-import {
-  type DatePickerUserConfig,
-  useDatePicker
-} from "@rehookify/datepicker";
 import { formatDate, formatTime } from "common/utils/datetime/format";
+import dynamic from "next/dynamic";
 import React, { useState } from "react";
 import { useFormContext } from "react-hook-form";
+
+const Modal = dynamic(() => import("./Modal"));
 
 interface DatePickerProps {
   initialDate?: Date;
@@ -29,48 +20,13 @@ interface DatePickerProps {
 
 const DatePicker: React.FC<DatePickerProps> = ({ initialDate }) => {
   const [open, setOpen] = useState<boolean>(false);
-  const [error, setError] = useState<boolean>(false);
   const {
     getValues,
     formState: { errors }
   } = useFormContext();
 
-  const config: DatePickerUserConfig = {
-    locale: {
-      weekday: "short"
-    }
-  };
-
-  const {
-    data: { selectedDates, calendars, weekDays },
-    propGetters: { nextMonthButton, previousMonthButton },
-    actions: { setDay }
-    // @ts-ignore
-  } = useDatePicker(config);
-
-  const selectedDate = selectedDates[0];
-
-  const { days, month, year } = calendars[0];
-
   const handleModalToggle = () => {
     setOpen(!open);
-  };
-
-  const handleModalClose = () => {
-    setOpen(false);
-  };
-
-  const handleModalDone = () => {
-    setError(false);
-    if (!selectedDate) return setError(true);
-
-    handleModalClose();
-  };
-
-  const handleRadioChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const [day, month, year] = e.target.value.split("/");
-    // Convert strings to ints
-    setDay(new Date(+year, +month - 1, +day));
   };
 
   return (
@@ -130,136 +86,7 @@ const DatePicker: React.FC<DatePickerProps> = ({ initialDate }) => {
         </Box>
       </Box>
 
-      <Modal open={open} onClose={handleModalClose} aria-label="Select date">
-        <ModalDialog
-          sx={{
-            overflow: "scroll",
-            maxHeight: "92%",
-            top: "unset",
-            pb: 8,
-            borderRadius: "xl",
-            borderBottomLeftRadius: 0,
-            borderBottomRightRadius: 0
-          }}
-          layout="fullscreen"
-        >
-          <Box component="form">
-            <Grid container spacing={2}>
-              <Grid
-                xs={12}
-                sx={{
-                  display: "flex",
-                  justifyContent: "space-between",
-                  alignItems: "center"
-                }}
-              >
-                <IconButton
-                  color="neutral"
-                  variant="plain"
-                  {...previousMonthButton()}
-                >
-                  <ChevronLeftRounded />
-                </IconButton>
-
-                <Box sx={{ textAlign: "center" }}>
-                  <Typography level="subtitle1">{year}</Typography>
-
-                  <Typography level="h5" component="p">
-                    {month}
-                  </Typography>
-                </Box>
-
-                <IconButton
-                  color="neutral"
-                  variant="plain"
-                  {...nextMonthButton()}
-                >
-                  <ChevronRightRounded />
-                </IconButton>
-              </Grid>
-
-              <Grid xs={12}>
-                <RadioGroup row onChange={handleRadioChange}>
-                  <Grid container columns={7} sx={{ width: "100%" }}>
-                    {weekDays.map((wd: string) => {
-                      const weekDay = wd.slice(0, 2);
-
-                      return (
-                        <Grid xs={1} key={`${month}-${weekDay}`} sx={{ mb: 1 }}>
-                          <Avatar
-                            size="sm"
-                            color="primary"
-                            variant="plain"
-                            sx={{ fontWeight: "xl" }}
-                          >
-                            {weekDay}
-                          </Avatar>
-                        </Grid>
-                      );
-                    })}
-
-                    {days.map((dpDay: any) => {
-                      let checked;
-
-                      return (
-                        <Grid key={`${month}-${dpDay.date}`} xs={1}>
-                          <Avatar
-                            size="sm"
-                            color="neutral"
-                            variant={dpDay.selected ? "soft" : "plain"}
-                          >
-                            <Radio
-                              value={dpDay.date}
-                              label={dpDay.day}
-                              disabled={!dpDay.inCurrentMonth}
-                              size="sm"
-                              color="neutral"
-                              variant="plain"
-                              overlay
-                              disableIcon
-                            />
-                          </Avatar>
-                        </Grid>
-                      );
-                    })}
-                  </Grid>
-                </RadioGroup>
-              </Grid>
-            </Grid>
-
-            {error && (
-              <Typography
-                startDecorator={<ErrorOutlineRounded />}
-                color="danger"
-                sx={{ mt: 3, fontWeight: "xl" }}
-              >
-                Select valid date
-              </Typography>
-            )}
-
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-                mt: 4,
-                gap: 2
-              }}
-            >
-              <Button
-                variant="plain"
-                color="neutral"
-                onClick={handleModalClose}
-              >
-                Cancel
-              </Button>
-              <Button color="neutral" onClick={handleModalDone} size="lg">
-                Done
-              </Button>
-            </Box>
-          </Box>
-        </ModalDialog>
-      </Modal>
+      <Modal open={open} setOpen={setOpen} />
     </>
   );
 };
