e4586da04220c97304de19a20f695aa20ed860d4
franciszekostrowski <franekostrowski@protonmail.com>
2023-01-15 16:53:42 +0100

    feat: onSubmit function now submits new dive to database

--- src/modules/Dives/New/index.tsx
+++ src/modules/Dives/New/index.tsx
@@ -1,4 +1,5 @@
 import type { User } from "@supabase/auth-helpers-nextjs";
+import type { Dive } from "common/types";
 import { supabase } from "common/utils/supabaseClient";
 import React from "react";
 import { useFormContext } from "react-hook-form";
@@ -12,16 +13,15 @@ interface NewProps {
 }
 
 const New: React.FC<NewProps> = ({ user }) => {
-  const { handleSubmit, getValues } = useFormContext<FormFields>();
-  const onSubmit = (data: any) => {
-    console.log(JSON.stringify(data, null, 2));
-  };
+  const { handleSubmit } = useFormContext<FormFields>();
 
-  const unusedOnSubmit = async (data: any) => {
-    const { error } = await supabase
+  const onSubmit = async (data: Dive) => {
+    const { error, status } = await supabase
       .from("dives")
-      .insert({ user_id: user.id, ...data });
-    if (error) console.error(error);
+      .insert({ userId: user.id, ...data });
+    console.log(data)
+    if (error) return console.error(error);
+    if (status === 201) console.log("Created!")
   };
 
   return (
