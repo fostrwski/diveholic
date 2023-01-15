1330fb9a4c52c6cc392fa337bb78f14f88da020b
franciszekostrowski <franekostrowski@protonmail.com>
2023-01-15 23:01:09 +0100

    feat: increase command timeout

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   projectId: "urunxf",
+  defaultCommandTimeout: 10000,
   component: {
     devServer: {
       framework: "next",
