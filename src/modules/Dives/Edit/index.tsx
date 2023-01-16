1927a95bc929118676459f22d967463d75f8cf4a
franciszekostrowski <franekostrowski@protonmail.com>
2023-01-16 03:13:56 +0100

    feat: start implementing edit functionality

--- src/modules/Dives/Edit/index.tsx
+++ src/modules/Dives/Edit/index.tsx
@@ -1,8 +1,6 @@
 import { useUser } from "@supabase/auth-helpers-react";
-import type { Dive } from "common/types";
 import { supabase } from "common/utils/supabaseClient";
-import { useRouter } from "next/router";
-import React, { useEffect } from "react";
+import React from "react";
 import { useFormContext } from "react-hook-form";
 
 import Form from "../components/Form";
@@ -10,34 +8,14 @@ import type { FormFields } from "../components/Form/types";
 import Header from "./Header";
 
 const Edit: React.FC = () => {
-  const { user } = useUser();
-  const router = useRouter();
-  const { id: diveId } = router.query;
   const onSubmit = (data: any) => console.log(data);
   const { handleSubmit } = useFormContext<FormFields>();
-
-  // useEffect(() => {
-  //   const getDiveById = async () => {
-  //     const { data, error } = await supabase
-  //       .from<Dive>("dives")
-  //       .select("*")
-  //       .match({ id: diveId });
-
-  //     if (error) return console.error(error);
-
-  //     if (data) {
-  //       const dive = data[0];
-  //     }
-  //   };
-
-  //   if (user) getDiveById();
-  // }, [user]);
 
   return (
     <>
       <Header />
 
-      <Form onSubmit={handleSubmit(onSubmit)} submitted={false}/>
+      <Form onSubmit={handleSubmit(onSubmit)} submitted={false} />
     </>
   );
 };
--- src/pages/dives/edit/[id].tsx
+++ src/pages/dives/edit/[id].tsx
@@ -1,15 +1,39 @@
-import { withPageAuth } from "@supabase/auth-helpers-nextjs";
+import {
+  supabaseServerClient,
+  withPageAuth
+} from "@supabase/auth-helpers-nextjs";
 import DefaultLayout from "common/layouts/Default";
+import type { Dive } from "common/types";
 import Edit from "modules/Dives/Edit";
 import defaultValues from "modules/Dives/components/Form/defaultValues";
 import { FormFields } from "modules/Dives/components/Form/types";
 import React from "react";
 import { FormProvider, useForm } from "react-hook-form";
 
-export const getServerSideProps = withPageAuth({ redirectTo: "/signin" });
+export const getServerSideProps = withPageAuth({
+  redirectTo: "/signin",
+  async getServerSideProps(ctx) {
+    const { data, error } = await supabaseServerClient(ctx)
+      .from<Dive>("dives")
+      .select("*")
+      .match({ id: ctx.params?.id });
 
-export default function EditPage() {
-  const methods = useForm<FormFields>({ defaultValues });
+    if (error) console.error(error);
+
+    if (data == null) return { props: { dive: null } };
+
+    const initialValues = {...defaultValues, ...data[0]}
+
+    return { props: { initialValues } };
+  }
+});
+
+interface EditPageProps {
+  initialValues: Dive;
+}
+
+export default function EditPage({ initialValues }: EditPageProps) {
+  const methods = useForm<FormFields>({ defaultValues: initialValues });
   return (
     <DefaultLayout>
       <FormProvider {...methods}>
