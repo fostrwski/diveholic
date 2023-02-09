d4b4b2341b9531e80b2f04364b61a7a80ff970dc
franciszekostrowski <franekostrowski@protonmail.com>
2023-02-09 10:07:53 +0100

    chore: remove unused search field

--- src/modules/Dives/index.tsx
+++ src/modules/Dives/index.tsx
@@ -1,6 +1,4 @@
-import SearchRounded from "@mui/icons-material/SearchRounded";
 import Grid from "@mui/joy/Grid";
-import TextField from "@mui/joy/TextField";
 import DiveCard from "common/components/DiveCard";
 import type { Dive } from "common/types";
 import React from "react";
@@ -11,13 +9,6 @@ interface DivesProps {
 
 const Dives: React.FC<DivesProps> = ({ data }) => (
   <>
-    <TextField
-      type="text"
-      placeholder="Search by location"
-      startDecorator={<SearchRounded />}
-      sx={{ mb: 4 }}
-    />
-
     <Grid container gap={2}>
       {data.map((dive: Dive) => (
         <Grid xs={12} key={dive.id}>
