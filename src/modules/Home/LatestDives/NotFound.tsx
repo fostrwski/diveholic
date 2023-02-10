9001002889860193e399f533d25fe3ea22739d32
franciszekostrowski <franekostrowski@protonmail.com>
2023-02-10 14:01:07 +0100

    style: responsiveness!

--- src/modules/Home/LatestDives/NotFound.tsx
+++ src/modules/Home/LatestDives/NotFound.tsx
@@ -1,20 +1,30 @@
+import Box from "@mui/joy/Box";
 import Button from "@mui/joy/Button";
 import Typography from "@mui/joy/Typography";
 import NextLink from "next/link";
 import React from "react";
 
 const NotFound: React.FC = () => (
-  <>
+  <Box
+    sx={{
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      gap: 1.2
+    }}
+  >
     <Typography component="p" level="subtitle1" textAlign="center">
       It's empty here. You probably haven't logged any dives yet 😱
     </Typography>
 
-    <NextLink href="/dives/new">
-      <Button component="a" variant="plain" size="sm" sx={{ mx: "auto" }}>
-        Add your first dive
-      </Button>
-    </NextLink>
-  </>
+    <div>
+      <NextLink href="/dives/new" passHref>
+        <Button component="a" variant="plain" size="sm" color="info">
+          Add your first dive
+        </Button>
+      </NextLink>
+    </div>
+  </Box>
 );
 
 export default NotFound;
