1f2fb288ef08e570f13f041d779b0a1caccc1ae5
franciszekostrowski <franekostrowski@protonmail.com>
2023-02-16 00:44:58 +0100

    fix: maybe this reloads i hope

--- src/common/layouts/Default/index.tsx
+++ src/common/layouts/Default/index.tsx
@@ -11,7 +11,8 @@ import { useUser } from "@supabase/auth-helpers-react";
 import generateInitials from "common/utils/generateInitials";
 import Image from "next/image";
 import NextLink from "next/link";
-import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import React, { useEffect, useMemo, useState } from "react";
 
 interface DefaultLayoutProps {
   children: React.ReactNode;
@@ -28,6 +29,20 @@ const DefaultLayout: React.FC<DefaultLayoutProps> = ({ children }) => {
       setInitials(generateInitials(firstName));
     }
   }, [user]);
+
+  const router = useRouter();
+
+  const handleRouteChangeComplete = useMemo(() => (url: string) => {
+    if (url === "/") router.replace("/");
+  }, [router])
+
+  useEffect(() => {
+    router.events.on("routeChangeComplete", handleRouteChangeComplete);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChangeComplete);
+    };
+  }, [router, handleRouteChangeComplete]);
 
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
@@ -92,7 +107,7 @@ const DefaultLayout: React.FC<DefaultLayoutProps> = ({ children }) => {
               <ListItemDecorator>
                 <DashboardRounded />
               </ListItemDecorator>
-                Home
+              Home
             </MenuItem>
           </NextLink>
 
